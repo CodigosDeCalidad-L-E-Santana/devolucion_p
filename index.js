cacf5c39b934716d93e9bbc4aab24116c11d5f6a
luissantana-b <luis.santana@banistmo.com>
2023-06-17 07:57:47 -0500

    v-base2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import puppeteer from 'puppeteer';
+
+async function openWebPage() {
+    const browser = await puppeteer.launch({
+        headless: 'new',
+        slowMo: 300
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www5.ipg-online.com/vt/login');
+
+    await browser.close();
+}
+
+openWebPage();
